33f26b52b8ea30cb576e3b56fbe233da08352c8f
Luc <luc.dangquang@rainmaker-labs.com>
2016-04-28 20:24:55 +0700

    cheat code for create roles

--- db/migrate/20160420084435_create_roles.rb
+++ db/migrate/20160420084435_create_roles.rb
@@ -1,11 +1,9 @@
 class CreateRoles < ActiveRecord::Migration
   def change
-    if (table_exists? :roles == false)
-      create_table :roles do |t|
-        t.references :user, index: true, foreign_key: true
-        t.string :role
-        t.timestamps null: false
-      end
+    create_table :roles do |t|
+      t.references :user, index: true, foreign_key: true
+      t.string :role
+      t.timestamps null: false
     end
 
   end
