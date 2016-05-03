0f55e5ab8c5913418ea419dc4b7183d6a8661809
Luc <luc.dangquang@rainmaker-labs.com>
2016-05-03 21:04:26 +0700

    update migration

--- db/migrate/20160419154405_create_roles.rb
+++ db/migrate/20160419154405_create_roles.rb
@@ -1,11 +1,16 @@
 class CreateRoles < ActiveRecord::Migration
   def change
-    create_table :roles do |t|
-      t.references :user, index: true, foreign_key: true
-      t.string :role
-      t.references :project, index: true, foreign_key: true
 
-      t.timestamps null: false
+    unless table_exists?
+      create_table :roles do |t|
+        t.references :user, index: true, foreign_key: true
+        t.string :role
+        t.references :project, index: true, foreign_key: true
+
+        t.timestamps null: false
+      end
+
     end
+
   end
 end
