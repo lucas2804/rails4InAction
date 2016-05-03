8a723c241b59dc0adf52d2cf63de4459eebd3015
Luc <luc.dangquang@rainmaker-labs.com>
2016-05-03 21:13:06 +0700

    resolve add foreign key for postgreSql add create_comment

--- db/migrate/20160501162548_create_comments.rb
+++ db/migrate/20160501162548_create_comments.rb
@@ -3,10 +3,9 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :text
       t.references :ticket, index: true, foreign_key: true
-      t.references :author, index: true, foreign_key: true
+      t.references :author, index: true
       t.timestamps null: false
     end
-
     add_foreign_key :comments, :users, column: :author_id
   end
 end
